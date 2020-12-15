47d52f6fa9b075b517d193776f6fd15c9107dc08
MikeM501 <marcos.castillo91@student.qcc.cuny.edu>
2020-12-15 11:56:54 -0500

    Marcos.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { ShippingComponent } from './shipping/shipping.component';
 import { FngAccordionComponent } from './fng-accordion/fng-accordion.component';
 import { GraphicDesignComponent } from './graphic-design/graphic-design.component';
 import { PortfolioLightboxComponent } from './portfolio-lightbox/portfolio-lightbox.component';
+import { CarsComponent } from './cars/cars.component';
 
 @NgModule({
   imports: [
@@ -68,7 +69,8 @@ import { PortfolioLightboxComponent } from './portfolio-lightbox/portfolio-light
     ShippingComponent,
     FngAccordionComponent,
     GraphicDesignComponent,
-    PortfolioLightboxComponent
+    PortfolioLightboxComponent,
+    CarsComponent
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
